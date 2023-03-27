64a5a50c1a805e10ec944c2f0e742e3442b10374
José Lucas <josluc2016@gmail.com>
2023-03-27 01:49:06 -0300

    feat: get schools function

--- src/controllers/school.controller.ts
+++ src/controllers/school.controller.ts
@@ -11,6 +11,42 @@ export const registrySchool = async (req: Request, res: Response) => {
     }
 }
 
-export const getSchools = (req: Request, res: Response) => { }
+export const getSchools = async (req: Request, res: Response) => {
+    try {
+        if (req.params.id) {
+            const data = await SchoolSchema.findOne({ _id: req.params.id })
+            return res.status(200).json(data)
+        }
+        const filter = {}
+        const queryData = req.query
+
+        const pageOptions = {
+            page: parseInt(queryData.page as string) || 0,
+            limit: parseInt(queryData.limit as string) || 10,
+        }
+
+        if (queryData.name) Object.assign(filter, { title: { $regex: queryData.title, $options: 'i' } })
+
+        const totalEntries = await SchoolSchema.find(filter).count()
+        const newsData = await SchoolSchema
+            .find(filter)
+            .skip(pageOptions.page * pageOptions.limit)
+            .limit(pageOptions.limit)
+
+        const data = {
+            data: newsData,
+            metadata: {
+                pageNumber: pageOptions.page,
+                pageSize: newsData.length,
+                totalEntries: totalEntries,
+                totalPages: Math.ceil(totalEntries / pageOptions.limit),
+            },
+        }
+
+        res.status(200).json(data)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
 export const updateSchool = (req: Request, res: Response) => { }
 export const deleteSchool = (req: Request, res: Response) => { }

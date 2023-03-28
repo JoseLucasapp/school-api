8046453e4cec2669268cf0bde83b4df5cd560a56
José Lucas <josluc2016@gmail.com>
2023-03-28 03:09:23 -0300

    feat: classroom delete, and update functions

--- src/controllers/classrooms.controller.ts
+++ src/controllers/classrooms.controller.ts
@@ -11,3 +11,21 @@ export const newClassroom = async (req: Request, res: Response) => {
         res.status(500).json(error)
     }
 }
+
+export const updateClassroom = async (req: Request, res: Response) => {
+    try {
+        await ClassroomSchema.updateOne({ _id: req.params.id, school_id: req.params.userId }, { $set: req.body }, { upsert: true, new: true })
+        res.status(200).json({ message: "Dados atualizados" })
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
+
+export const deleteClassroom = async (req: Request, res: Response) => {
+    try {
+        await ClassroomSchema.deleteOne({ _id: req.params.id, school_id: req.params.userId })
+        res.status(200).json({ message: "Dados apagados" })
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
--- src/routes/classrooms.routes.ts
+++ src/routes/classrooms.routes.ts
@@ -1,8 +1,10 @@
 import { NextFunction, Request, Response, Router } from "express";
-import { newClassroom } from "../controllers/classrooms.controller";
+import { deleteClassroom, newClassroom, updateClassroom } from "../controllers/classrooms.controller";
 import { UserTypeEnum } from "../helpers/types";
 import { validadeJwt } from "../middleware/validateLogin.middleware";
 
 export default (router: Router) => {
     router.post('/classroom', [(req: Request, res: Response, next: NextFunction) => validadeJwt(req, res, next, [UserTypeEnum.SCHOOL])], newClassroom)
+    router.post('/classroom/:id', [(req: Request, res: Response, next: NextFunction) => validadeJwt(req, res, next, [UserTypeEnum.SCHOOL])], updateClassroom)
+    router.post('/classroom/:id', [(req: Request, res: Response, next: NextFunction) => validadeJwt(req, res, next, [UserTypeEnum.SCHOOL])], deleteClassroom)
 }

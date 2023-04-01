4d52192210f9a2d61ceac6b6e5510c89f1af34b6
José Lucas <josluc2016@gmail.com>
2023-04-01 18:32:31 -0300

    fix: getbyid route

--- src/routes/classrooms.routes.ts
+++ src/routes/classrooms.routes.ts
@@ -1,11 +1,12 @@
 import { NextFunction, Request, Response, Router } from "express";
-import { deleteClassroom, getClassrooms, newClassroom, updateClassroom } from "../controllers/classrooms.controller";
+import { deleteClassroom, getClassroomById, getClassrooms, newClassroom, updateClassroom } from "../controllers/classrooms.controller";
 import { UserTypeEnum } from "../helpers/types";
 import { validadeJwt } from "../middleware/validateLogin.middleware";
 
 export default (router: Router) => {
     router.post('/classroom', [(req: Request, res: Response, next: NextFunction) => validadeJwt(req, res, next, [UserTypeEnum.SCHOOL])], newClassroom)
-    router.get('/classroom/:id', [(req: Request, res: Response, next: NextFunction) => validadeJwt(req, res, next, [UserTypeEnum.SCHOOL, UserTypeEnum.STUDENT, UserTypeEnum.TEACHER, UserTypeEnum.ADMIN])], getClassrooms)
+    router.get('/classroom', [(req: Request, res: Response, next: NextFunction) => validadeJwt(req, res, next, [UserTypeEnum.SCHOOL, UserTypeEnum.STUDENT, UserTypeEnum.TEACHER, UserTypeEnum.ADMIN])], getClassrooms)
+    router.get('/classroom/:id', [(req: Request, res: Response, next: NextFunction) => validadeJwt(req, res, next, [UserTypeEnum.SCHOOL, UserTypeEnum.STUDENT, UserTypeEnum.TEACHER, UserTypeEnum.ADMIN])], getClassroomById)
     router.put('/classroom/:id', [(req: Request, res: Response, next: NextFunction) => validadeJwt(req, res, next, [UserTypeEnum.SCHOOL])], updateClassroom)
     router.delete('/classroom/:id', [(req: Request, res: Response, next: NextFunction) => validadeJwt(req, res, next, [UserTypeEnum.SCHOOL])], deleteClassroom)
 }

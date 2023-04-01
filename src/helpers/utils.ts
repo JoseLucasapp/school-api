1ae663b3e4feaec8ccc8d374b16fd6e2c9373a65
José Lucas <josluc2016@gmail.com>
2023-04-01 18:33:54 -0300

    fix: getbyid function

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -29,10 +29,8 @@ export const createSchoolData = async (res: Response, Schema: any, body: any) =>
 
 export const getDataById = async (req: Request, res: Response, Schema: any) => {
     try {
-        if (req.params.id) {
-            const data = await Schema.findOne({ _id: req.params.id, school_id: req.params.userId })
-            return res.status(200).json(data)
-        }
+        const data = await Schema.findOne({ _id: req.params.id, school_id: req.params.userId })
+        return res.status(200).json(data)
     } catch (error) {
         res.status(500).json(error)
     }

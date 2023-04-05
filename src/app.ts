a57dd2c1f0368e6506e9ea1fe96349c6c511529d
José Lucas <josluc2016@gmail.com>
2023-04-05 13:58:13 -0300

    feat: limiter

--- src/app.ts
+++ src/app.ts
@@ -4,13 +4,20 @@ import './config/db.config'
 import routes from "./routes"
 import express, { Request, Response } from "express"
 import cors from "cors"
+import { rateLimit } from "express-rate-limit"
 
 const app = express()
 const route = express.Router()
 const PORT = process.env.PORT
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+    message: "Too many requests, please try again later"
+})
 
 app.use(express.json())
 app.use(cors())
+app.use(limiter)
 app.use('/api', route)
 routes(route)
 
